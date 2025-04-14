1304b52e1fd40dbec9d2628638f5a21d3c2268b4
Triskell95 <tdasilva@et.esiea.fr>
2025-04-14 12:55:16 +0200

    Remove build folder from repo

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MyTradingView_FILE C:/Users/Thibaut/OneDrive/Documents/QtProjects/MyTradingView/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/MyTradingView.exe)
-set(__QT_DEPLOY_TARGET_MyTradingView_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MyTradingView_RUNTIME_DLLS E:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/deploy_MyTradingView_75ccfad8ef.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/Thibaut/OneDrive/Documents/QtProjects/MyTradingView/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/MyTradingView-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/Users/Thibaut/OneDrive/Documents/QtProjects/MyTradingView/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/MyTradingView.exe
-    GENERATE_QT_CONF
-)
